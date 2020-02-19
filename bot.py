07098df0dd5fb1bd48b92ce531c3ec10b24f96cf
yuhao_huang <t106362525@ntut.org.tw>
2020-02-19 13:31:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,25 @@ bot = commands.Bot(command_prefix='[')
 
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    print(">>bot is online<<")
 
-bot.run('Njc4ODc0MTUzNTU3Njg4MzUy.XkpI2g.FqQdzetCm7YeLGCFt6Ih7Q_-vKc')
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join')
+    chaanel = bot.get_channel(679208154034339860)
+    await chaanel.send(f'{member} join')
+
+@bot.event
+async def on_member_leave(member):
+    print(f'{member} leave')
+    chaanel = bot.get_channel(679208154034339860)
+    await chaanel.send(f'{member} leave')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
 
+
+
+bot.run('Njc4ODc0MTUzNTU3Njg4MzUy.Xkydkw.ldoixSSBY_AvAl1lp_W4jo7WbIA')
